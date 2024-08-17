4125da14d659cebd87ae0a7fc2ae78c53095b8b8
amandadhwal <amandadhwal911@gmail.com>
2024-08-17 12:32:09 +0530

    ui complete

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include('header.php');?>
+            <h2>All Student</h2>
+                        
+                    <table class="table table-hover table-bordered table-striped">
+                        <thead> 
+                            <tr class="bg-primary">
+                                <th>ID</th>
+                                <th>first_name</th>
+                                <th>last_name</th>
+                                <th>age</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr >
+                                <td>1</td>
+                                <td>Aman</td>
+                                <td>Dadhwal</td>
+                                <td>24</td>
+                            </tr>
+                            <tr >
+                                <td>2</td>
+                                <td>Pragya</td>
+                                <td>Gupta</td>
+                                <td>24</td>
+                            </tr>
+                        </tbody>
+
+                    </table>
+
+                    <?php include('footer.php');?>
+
+        
